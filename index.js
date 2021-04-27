0c15b4c55bc3bfa268e99f412c25e179caed9242
fitzgebt <fitzgebt@gmail.com>
2021-04-27 12:33:31 -0700

    commit tree 6bbe99d797d14fc74466d5219aa6f2f59577be45

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here
+class Breakfast {
+    constructor(meal, drink) {
+        this.food = meal
+        this.drink = drink
+    }
+}
+
+class Lunch {
+    constructor(app, meal, drink) {
+        this.salad = app;
+        this.soup = meal
+        this.drink = drink
+    }
+
+}
+
+class Dinner {
+    constructor(salad, soup, food, dessert) {
+        this.salad = salad
+        this.soup = soup
+        this.entree = food
+        this._dessert = dessert
+    }
+
+}
